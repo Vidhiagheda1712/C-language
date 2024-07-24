76994641a98273f5f9990658c18a195681d5dd9c
VidhiAgheda <168967567+Vidhiagheda1712@users.noreply.github.com>
2024-07-24 21:19:41 +0530

    Update q3.c

--- PR-2/q3.c
+++ PR-2/q3.c
@@ -8,19 +8,22 @@ main() {
     printf("Enter your score : ");
     scanf("%d", &score);
 
-    if (score>=80 && score<=100) {
+     if (score>=81 && score<=100) {
         printf(" your grade is A... Excellent work! You are eligible for the next level.\n");
     }
-	  else if (score>=70 && score<=80) {
+	  else if (score>=71 && score<=80) {
         printf("your grade is B...Congratulations! You are eligible for the next level.\n");
     } 
-	else if (score>=60 && score<=70) {
+	else if (score>=61 && score<=70) {
         printf("your grade is C...Congratulations! You are eligible for the next level.\n");
     }
-	else if (score>=50 && score<=60) {
+	else if (score>=51 && score<=60) {
         printf("your grade is D...Congratulations! You are eligible for the next level.\n");
     }  
-    	else if (score <50) {
+	else if (score>=41 && score<=50) {
+        printf("your grade is E...Congratulations! You are eligible for the next level.\n");
+    }
+    	else if (score =<40) {
         printf("your grade is F...Please try again next time\n");
     } 
 	else {
