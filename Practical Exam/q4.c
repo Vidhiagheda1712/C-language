f5fb905d52ce6966f2c1a05d1d62bf403b61f0d8
VidhiAgheda <168967567+Vidhiagheda1712@users.noreply.github.com>
2024-09-06 10:14:27 +0530

    Update q4.c

--- Practical Exam/q4.c
+++ Practical Exam/q4.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 
-    leapyear(){
 
-        if()
+int LeapYear(int year) {
+    if (year % 4 == 0) {
+      return 1; 
+    } 
+    else {
+        return 0; 
+    }
+}
+
+
+int main() {
+    int start, end;
+
+    
+    printf("Enter the first number: ");
+    scanf("%d", &start);
+    printf("Enter the second number: ");
+    scanf("%d", &end);
+
+    printf("Leap years between %d and %d are:\n", start, end);
+
+    int year = start;
+
+    while (year <= end) {
+        if (LeapYear(year)) {
+            printf("%d ",year);
+        }
+        year++;
     }
 
-void main(){
-   
-   int start,end;
-
-
-   printf("Enter Start Year :");
-   scanf("%d",&start);
-   printf("Enter Start Year :");
-   scanf("%d",&start);
+    printf("\n");
 
    
-     
 }
